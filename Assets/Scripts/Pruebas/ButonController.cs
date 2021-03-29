f9ad8a67126b35cb3b7fea8f48ce9431cd66bee4
Diego Camilo Herrera Ferrer <danthe1996@gmail.com>
2021-03-29 16:15:47 -0500

    Ra button
    Ra button para enviar datos

---
+++ Assets/Scripts/Pruebas/ButonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButonController : MonoBehaviour
+{
+    public Button[] Botones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ActiveButon()
+    {
+        for (int i = 0; i < Botones.Length; i++)
+        {
+            Botones[i].interactable = true;
+        }
+        
+    }
+    public void DesctiveButon()
+    {
+        for (int i = 0; i < Botones.Length; i++)
+        {
+            Botones[i].interactable = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
